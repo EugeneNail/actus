78c7724e5058c5705216e2b07d47857be7ffb920
Евгений Гвоздиков <96439713+EugeneNail@users.noreply.github.com>
2024-06-24 11:46:20 +0300

    #22 test (activities): add test for index route

--- tests/Feature/Collection/IndexTest.php
+++ tests/Feature/Collection/IndexTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Collection;
 
+use App\Models\Activity;
 use App\Models\Collection;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -14,18 +15,32 @@ class IndexTest extends AuthorizedTestCase
 {
     public function test_index(): void
     {
-        $collections = Collection::factory()->count(rand(0, 20))->for(User::first())->create();
+        $user = User::first();
+        $collections = Collection::factory()
+            ->count(rand(0, 20))
+            ->for($user)
+            ->has(
+                Activity::factory()
+                    ->for($user)
+                    ->count(rand(0, 20))
+            )
+            ->create();
 
         $this
             ->assertDatabaseCount('collections', $collections->count())
             ->get(route('collections.index'))
             ->assertInertia(function (AssertableInertia $assert) use ($collections) {
-                foreach ($collections as $i => $collection) {
-                    $assert->component('Collection/Index')
-                        ->has('collections', $collections->count())
-                        ->where("collections.$i.id", $collection->id)
-                        ->where("collections.$i.name", $collection->name)
-                        ->where("collections.$i.color", $collection->color);
+                foreach ($collections as $ci => $collection) {
+                    foreach ($collection->activities as $ai => $activity) {
+                        $assert->component('Collection/Index')
+                            ->has('collections', $collections->count())
+                            ->has("collections.$ci.activities", $collection->activities->count())
+                            ->where("collections.$ci.id", $collection->id)
+                            ->where("collections.$ci.name", $collection->name)
+                            ->where("collections.$ci.color", $collection->color)
+                            ->where("collections.$ci.activities.$ai.name", $activity->name)
+                            ->where("collections.$ci.activities.$ai.icon", $activity->icon);
+                    }
                 }
             });
     }
