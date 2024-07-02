579234a59aaf7124aaa89d6633240f0b4aab6445
Евгений Гвоздиков <96439713+EugeneNail@users.noreply.github.com>
2024-07-02 18:01:58 +0300

    #48 feat: increase diary column max length

---
+++ database/migrations/2024_07_02_145923_increase_diary_length.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('entries', function (Blueprint $table) {
+            $table->string('diary', 5000)->change();
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::table('entries', function (Blueprint $table) {
+            $table->string('diary')->change();
+        });
+    }
+};
