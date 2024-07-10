052ecb9bcc10b1a6708f2931a009f8f780f262c4
Евгений Гвоздиков <96439713+EugeneNail@users.noreply.github.com>
2024-07-10 20:46:12 +0300

    #62 feat: hide user info from Inertia data import

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,11 @@ class User extends Authenticatable
     ];
 
     protected $hidden = [
+        'name',
+        'email',
+        'email_verified_at',
+        'created_at',
+        'updated_at',
         'password',
         'remember_token',
     ];
