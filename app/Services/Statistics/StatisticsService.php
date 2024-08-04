abba6b0afb6733ca84bb797eeb142dc290eb6c07
Евгений Гвоздиков <96439713+EugeneNail@users.noreply.github.com>
2024-08-04 19:04:56 +0300

    #31 feat (statistics): add NodeEntry entity

--- app/Services/Statistics/StatisticsService.php
+++ app/Services/Statistics/StatisticsService.php
@@ -4,9 +4,9 @@ namespace App\Services\Statistics;
 
 use App\Models\Entry;
 use App\Models\Support\NodeActivity;
+use App\Models\Support\NodeEntry;
 use App\Models\User;
 use Illuminate\Support\Carbon;
-use InvalidArgumentException;
 
 class StatisticsService implements StatisticsServiceInterface
 {
@@ -37,5 +37,17 @@ class StatisticsService implements StatisticsServiceInterface
         }
 
         return $nodes;
+    }
+
+
+    /** @return array<NodeEntry> */
+    public function getEntryNodes(User $user, int $daysAgo): array
+    {
+        return $user
+            ->entries()
+            ->where('date', '>', (new Carbon())->subDays($daysAgo))
+            ->get()
+            ->map(fn(Entry $entry) => new NodeEntry($entry->mood, $entry->weather, $entry->date))
+            ->toArray();
     }
 }
